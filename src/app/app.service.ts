46e8b84d36914b1321ab9b6d778d694c0b4e0ce1
Daniel Oram <danieloramnz@gmail.com>
2017-01-16 20:09:21 +1300

    setup service with observables

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class AppService {
 
+  //observable variables
+  private contentSource = new Subject<string>();
+  private contentStream = this.contentSource.asObservable();
+
+  private contentString: string;
+
   constructor() { }
+
+  setMessage(message: string) {
+    this.contentSource.next(message);
+    this.contentString = message;
+  }
 
 }
